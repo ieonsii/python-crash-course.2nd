02f1dc57a66113d45406933a4ba0787a6b6391e7
Adrian Leones <leonsio192@gmail.com>
2020-07-13 21:13:32 +1000

    Fixed your_own_list

--- chapter_3/your_own_list.py
+++ chapter_3/your_own_list.py
@@ -3,8 +3,8 @@ cars = ['tesla', 'mercedes', 'hyundai']
 statement = f"The dream car is a {cars[0].title()}."
 print(statement)
 
-statement = f"The ideal car is a {cars[1].title()}."
+statement = f"The ideal car is a {cars[-1].title()}."
 print(statement)
 
-statement = f"However, we might be able to get a {cars[2].title()}!"
+statement = f"However, we might be able to get a {cars[1].title()}!"
 print(statement)
